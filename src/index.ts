4ef42c14e375bf4b86fa34f839fc61c024336078
Ryan Blace <reblace@gmail.com>
2018-12-12 16:43:54 -0500

    Exposing directives

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { LeafletD3Module } from './leaflet-d3/leaflet-d3.module';
+
+export { LeafletHexbinDirective } from './leaflet-d3/hexbin/leaflet-hexbin.directive';
+export { LeafletPingDirective } from './leaflet-d3/ping/leaflet-ping.directive';
+export { LeafletPingEvent } from './leaflet-d3/ping/leaflet-ping-event.model';
